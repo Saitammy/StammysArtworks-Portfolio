aa65c6cf6c0cfdb705dbc419399b41d73ec119cd
Saitammy <kishansampreet68@gmail.com>
2026-09-15 11:57:13 +0530

    Removed things not needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
-import WISA from "./sections/WISA";
-import Contact from "./sections/Contact";
 import Reviews from "./sections/Reviews";
-import Footer from "./sections/Footer";
 import Home from "./sections/Home";
-import Portfolio from "./sections/Portfolio";
 
 export default function App() {
   return (
@@ -13,11 +9,6 @@ export default function App() {
       <CustomCursor />
       <Navbar />
       <Home />
-      <WISA />
-      <Portfolio />
-      <Reviews />
-      <Contact />
-      <Footer />
     </div>
   );
 }
